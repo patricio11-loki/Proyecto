e7dce621a52f0df7d17890311498a57a9cb0d526
Juan <jc11093@gmail.com>
2024-11-27 22:28:35 -0500

    cambios en el index y en el html

--- assests/js/carrito.js
+++ assests/js/carrito.js
@@ -1,30 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Variables - base de datos
+//DOM
+document.addEventListener('DOMContentLoaded', () => { 
+    // Variables
     const baseDeDatos = [
         {
             id: 1,
-            nombre: 'Jarro hecho a mano, con estilo del caribe',
+            nombre: 'Jarro al 100% en barro',
             precio: 250000,
-            image: 'assests/img/jarro1.png'
+            imagen: 'assests/img/jarro1.png',
+            categoria: 'sombreros'
         },
         {
             id: 2,
-            nombre: 'Mochila hecha a mano por nuestras mejores diseñadores',
-            precio: 80000,
-            image: 'assests/img/mochila1.png'
+            nombre: 'Mochila al 100%',
+            precio: 150000,
+            imagen: 'assests/img/mochila1.png',
+            categoria: 'sombreros'
         },
         {
             id: 3,
-            nombre: 'Cadena elaborada en oro extraído del caribe',
-            precio: 100000,
-            image: 'assests/img/producto3.jpg'
+            nombre: 'Vajilla 100%',
+            precio: 250000,
+            imagen: 'assests/img/vajilla1.png',
+            categoria: 'sombreros'
         },
         {
             id: 4,
-            nombre: 'Vajilla 100% en barro',
-            precio: 150000,
-            image: 'assests/img/vajilla1.png'
+            nombre: 'cadena multiuso',
+            precio: 120000,
+            imagen: 'assests/img/producto2.jpg',
+            categoria: 'mochilas'
         },
+        {
+            id: 5,
+            nombre: 'jarroens 100%',
+            precio: 120000,
+            imagen: 'assests/img/producto1.jpg',
+            categoria: 'bolsos'
+        },
+        {
+            id: 6,
+            nombre: 'pulseras 2000',
+            precio: 120000,
+            imagen: 'assests/img/producto4.jpg',
+            categoria: 'hamacas'
+        }
     ];
 
     let carrito = [];
@@ -32,178 +51,154 @@ document.addEventListener('DOMContentLoaded', () => {
     const DOMitems = document.querySelector('#items');
     const DOMcarrito = document.querySelector('#carrito');
     const DOMtotal = document.querySelector('#total');
-    const DOMbotonvaciar = document.querySelector('#boton-vaciar');
+    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
     const miLocalStorage = window.localStorage;
     const filtroSelect = document.getElementById("filtro");
-    
-    // Función para renderizar los productos
+
+    // Funciones
+
     function renderizarProductos() {
         DOMitems.innerHTML = "";
 
         const filtro = filtroSelect.value;
-        const productosFiltrados = baseDeDatos.filter(producto =>
+        const productosFiltrados = baseDeDatos.filter(producto => 
             filtro === "todas" || producto.categoria === filtro
         );
-        
+
         productosFiltrados.forEach((info) => {
-            // Crear el nodo de la tarjeta
             const miNodo = document.createElement('div');
             miNodo.classList.add('card', 'col-sm-4');
-
-            // Crear el cuerpo de la tarjeta
+            
             const miNodoCardBody = document.createElement('div');
             miNodoCardBody.classList.add('card-body');
-
-            // Crear el título del producto
+            
             const miNodoTitle = document.createElement('h6');
             miNodoTitle.classList.add('card-title');
             miNodoTitle.textContent = info.nombre;
-
-            // Crear la imagen del producto
+            
             const miNodoImagen = document.createElement('img');
             miNodoImagen.classList.add('img-fluid');
-            miNodoImagen.setAttribute('src', info.image);
-
-            // Crear el precio del producto
+            miNodoImagen.setAttribute('src', info.imagen);
+            
             const miNodoPrecio = document.createElement('p');
             miNodoPrecio.classList.add('card-text');
-            miNodoPrecio.textContent = `${divisa} ${info.precio}`;
-
-            // Crear el botón de agregar al carrito
+            miNodoPrecio.textContent = `${info.precio}${divisa}`;
+            
             const miNodoBoton = document.createElement('button');
             miNodoBoton.classList.add('btn', 'btn-primary');
             miNodoBoton.textContent = 'Agregar';
-            miNodoBoton.setAttribute('marcador', producto.id);
+            miNodoBoton.setAttribute('marcador', info.id);
             miNodoBoton.addEventListener('click', anadirProductoAlCarrito);
 
-            // Insertar los elementos en la tarjeta
             miNodoCardBody.appendChild(miNodoImagen);
             miNodoCardBody.appendChild(miNodoTitle);
             miNodoCardBody.appendChild(miNodoPrecio);
             miNodoCardBody.appendChild(miNodoBoton);
             miNodo.appendChild(miNodoCardBody);
-            
-
-            // Insertar la tarjeta en el DOM
             DOMitems.appendChild(miNodo);
         });
     }
 
-    let visitas = localStorage.getItem('contadordeVisitas');
-    if(!visitas){
-        visitas=0;
-    }
-    visitas++;
-    
+// Obtén el contador del almacenamiento local
+let visitas = localStorage.getItem('contadorVisitas');
 
-localStorage.setItem('contadorVisitas', visitas)
+// Si no hay visitas almacenadas, inicializa a 0
+if (!visitas) {
+    visitas = 0;
+}
 
-document.getElementById('contador').textContent = visitas;
+// Incrementa el contador
+visitas++;
 
-    // Función para agregar productos al carrito
+// Guarda el nuevo contador en el almacenamiento local
+localStorage.setItem('contadorVisitas', visitas);
+
+// Muestra el contador en la página
+//document.getElementById('contador').textContent = visitas;
+
     function anadirProductoAlCarrito(evento) {
-        // Añadir el producto al carrito
         carrito.push(evento.target.getAttribute('marcador'));
-
-        // Renderizar el carrito y actualizar el valor
         renderizarCarrito();
-        guardarCarritoDeLocalStorage();
+        guardarCarritoEnLocalStorage();
         handleCarritoValue(carrito.length);
     }
 
-    // Función para manejar el valor del carrito en el contenedor
     function handleCarritoValue(value) {
         const carritoContainer = document.getElementById("carrito-value");
         carritoContainer.textContent = `${value}`;
     }
 
-    // Función para renderizar el carrito
     function renderizarCarrito() {
-        // Vaciamos el carrito en el DOM
         DOMcarrito.textContent = '';
-
-        // Quitamos los duplicados de los productos en el carrito
         const carritoSinDuplicados = [...new Set(carrito)];
 
-        carritoSinDuplicados.forEach((itemId) => {
-            // Obtenemos el producto de la base de datos
-            const producto = baseDeDatos.find((item) => item.id === parseInt(itemId));
+        carritoSinDuplicados.forEach((item) => {
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
 
-            // Contamos las unidades del producto
-            const numeroUnidadesItem = carrito.reduce((total, id) => (id === itemId ? total + 1 : total), 0);
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                return itemId === item ? total += 1 : total;
+            }, 0);
 
-            // Crear el nodo del producto en el carrito
             const miNodo = document.createElement('li');
             miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-            miNodo.textContent = `${numeroUnidadesItem} x ${producto.nombre} - ${divisa} ${producto.precio}`;
-
-            // Crear el botón para eliminar el producto del carrito
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+            
             const miBoton = document.createElement('button');
             miBoton.classList.add('btn', 'btn-danger', 'mx-5');
             miBoton.textContent = 'X';
             miBoton.style.marginLeft = '1rem';
-            miBoton.dataset.item = itemId;
+            miBoton.dataset.item = item;
             miBoton.addEventListener('click', borrarItemCarrito);
-
-            // Insertar el botón en el nodo
+            
             miNodo.appendChild(miBoton);
-
-            // Insertar el nodo en el carrito
             DOMcarrito.appendChild(miNodo);
         });
+        DOMtotal.textContent = calcularTotal();
+    }
 
-            DOMtotal.textContent = calcularTotal();
-        }
-
-    
-
-    
-
-    // Función para borrar un producto del carrito
     function borrarItemCarrito(evento) {
         const id = evento.target.dataset.item;
-
-        // Filtrar el carrito eliminando el producto
-        carrito = carrito.filter((carritoId) => carritoId !== id);
-
-        // Renderizar el carrito y actualizar el valor
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
         renderizarCarrito();
+        guardarCarritoEnLocalStorage();
         handleCarritoValue(carrito.length);
     }
 
-   
-
-    function calcularTotal(){
+    function calcularTotal() {
         return carrito.reduce((total, item) => {
-            const miItem= baseDeDatos.filter((itemBaseDatos)=>{
-                return itemBaseDatos.id ===parseInt(item);
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
             });
             return total + miItem[0].precio;
-         }, 0).toFixed(2);
-    } 
-    
+        }, 0).toFixed(2);
+    }
 
-    function vaciarCarrito(){
-        carrito= [];
+    function vaciarCarrito() {
+        carrito = [];
         renderizarCarrito();
         localStorage.clear();
     }
 
-    function guardarCarritoDeLocalStorage(){
+    function guardarCarritoEnLocalStorage() {
         miLocalStorage.setItem('carrito', JSON.stringify(carrito));
     }
 
-    function cargarCarritoDeLocalStorage(){
-        if (miLocalStorage.getItem('carrito') !==null) {
+    function cargarCarritoDeLocalStorage() {
+        if (miLocalStorage.getItem('carrito') !== null) {
             carrito = JSON.parse(miLocalStorage.getItem('carrito'));
             handleCarritoValue(carrito.length);
         }
     }
 
-    DOMbotonvaciar.addEventListener('click', vaciarCarrito);
+    // Eventos
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
     filtroSelect.addEventListener('change', renderizarProductos);
 
-    // Inicializar la aplicación
+    // Inicio
     cargarCarritoDeLocalStorage();
     renderizarProductos();
     renderizarCarrito();
